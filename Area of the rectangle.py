02f22876080a260b45d5064f13b0a1226480c625
Lalo Dominguez <143583115+WrittenInFire@users.noreply.github.com>
2023-08-30 12:58:13 -0700

    Create Area of the rectangle.py
    This simple program calculates the area of the rectangle. With conditional 'if' 'else' statements that determine whether or not the area is greater than equal to or less than 100

---
+++ Area of the rectangle.py
@@ -0,0 +1,9 @@
+length = float(input("Enter the length of the rectangle:"))
+width = float(input("Enter the width of the rectangle:"))
+
+area = length * width 
+
+if area > 100:
+  print("The area is greater than 100 square units")
+else:
+  print("The area is less than or equal to 100 square units")
